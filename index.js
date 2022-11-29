b1ba59d5346f190b437197c5024ce9735b90ea83
AlexFly93 <muhabon93@gmail.com>
2022-11-29 12:23:55 +0200

    Add completed homework

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 "use strict";
 
-
+const sentence = "     to         BE       oR   nOt        To     bE ";
+function toJadenCase(str) {
+  // debugger
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .filter((word) => word !== "")
+    .map((word) => word[0].toUpperCase() + word.substring(1))
+    .join(" ");
+}
+console.log(toJadenCase(sentence)); 
 
